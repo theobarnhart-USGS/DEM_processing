42e5deb8daebf0a94791157287fa77f7938190f2
Theodore <tbarnhart@usgs.gov>
2018-07-10 15:44:16 -0600

    download NE data

---
+++ get_NE.sh
@@ -0,0 +1,7 @@
+cd ./data/NE
+
+wget --limit-rate=300k ftp://dnrftp.dnr.ne.gov/Pub/data/dems/LiDAR_SC_2009/yorkdem.zip
+
+#wget --limit-rate=300k ftp://dnrftp.dnr.ne.gov/Pub/data/dems/LiDAR_SC_2009/sewarddem.zip
+
+wget --limit-rate=300k ftp://dnrftp.dnr.ne.gov/Pub/data/dems/LiDAR_SC_2009/hamiltondem.zip
