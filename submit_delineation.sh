4e321379889f5f342dde581ab8ae381b54bb177d
Theodore <tbarnhart@usgs.gov>
2018-08-25 15:59:25 -0600

    additional time for extra steps.

--- submit_delineation.sh
+++ submit_delineation.sh
@@ -6,13 +6,13 @@
 #SBATCH -n 1            # number of cores needed
 #SBATCH -p normal                         # the partition you want to use, for this case prod is best
 #SBATCH --account=wymtwsc        # your account
-#SBATCH --time=86:00:00           # Overestimated time
+#SBATCH --time=100:00:00           # Overestimated time
 #SBATCH --mail-type=ALL         # Send email on all events
 #SBATCH --mail-user=tbarnhart@usgs.gov
 #SBATCH  -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)
-#SBATCH --mem=15000
+#SBATCH --mem=100000
 
 source activate py27 # load correct python environment
 module load gis/grass-7.4-spack # load GRASS
 
-grass74 ./grass/reg${1}/PERMANENT --exec python2 ./extract_watersheds.py ${1} # open grass and run the program
+grass74 ./grass/reg${1}/PERMANENT --exec python2 -u ./extract_watersheds_lowmem.py ${1} # open grass and run the program
