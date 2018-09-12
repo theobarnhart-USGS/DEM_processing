1eaa1a47b52da3d6da9918d1a3328f41de82b69d
Theodore Barnhart <tbarnhart@usgs.gov>
2018-09-12 15:39:31 -0600

    added rasterize for norm_storage and npd occurances

--- rasterize_vectors.sh
+++ rasterize_vectors.sh
@@ -1,1 +1,5 @@
 gdal_rasterize -te -2493045.000 177285.000 2342655.000 3310005.000 -tr 30 30 -ot GTiff -co "PROFILE=GeoTIFF" -co "TILED=YES" -co "SPARSE_OK=TRUE" -co "COMPRESS=LZW" -co "NUM_THREADS=ALL_CPUS" -init 0.0 -add -a NID_STORAG -l points nid09ad_jun11.gdb nid_storage.tiff
+
+gdal_rasterize -te -2493045.000 177285.000 2342655.000 3310005.000 -tr 30 30 -ot GTiff -co "PROFILE=GeoTIFF" -co "TILED=YES" -co "SPARSE_OK=TRUE" -co "COMPRESS=LZW" -co "NUM_THREADS=ALL_CPUS" -init 0.0 -add -a NORM_STORAG -l points nid09ad_jun11.gdb nid_normstor.tiff
+
+gdal_rasterize -te -2493045.000 177285.000 2342655.000 3310005.000 -tr 30 30 -ot GTiff -co "PROFILE=GeoTIFF" -co "TILED=YES" -co "SPARSE_OK=TRUE" -co "COMPRESS=LZW" -co "NUM_THREADS=ALL_CPUS" -init 0.0 -add -a F_SCALE -l points us_major_npd.gdb npd_occur.tiff
