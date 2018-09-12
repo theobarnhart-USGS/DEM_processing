a79374e694f5948b67f49177a8eeb5c869d70df0
Theodore Barnhart <tbarnhart@usgs.gov>
2018-09-12 14:44:45 -0600

    initial code to rasterize point data from NID

---
+++ rasterize_vectors.sh
@@ -0,0 +1,1 @@
+gdal_rasterize -te -2493045.000 177285.000 2342655.000 3310005.000 -tr 30 30 -a_srs EPSG:42303 -ot GTiff -co "PROFILE=GeoTIFF" -co "TILED=YES" -co "SPARSE_OK=TRUE" -co "COMPRESS=LZW" -co "NUM_THREADS=ALL_CPUS" -init 0.0 -add -a NID_STORAG -l points nid09ad_jun11.gdb nid_storage.tiff
