1813a370eec24dc61fbd9d28b2c2964b7d8aea18
Theodore Barnhart <tbarnhart@usgs.gov>
2018-10-05 11:24:48 -0600

    added routine for nlcd data

--- reproject_cpg_data.sh
+++ reproject_cpg_data.sh
@@ -22,3 +22,7 @@ ssrs="EPSG:42303"
 #ssrs='PROJCS["Sphere_ARC_INFO_Lambert_Azimuthal_Equal_Area",GEOGCS["GCS_Sphere_ARC_INFO",DATUM["D_Sphere_ARC_INFO",SPHEROID["Sphere_ARC_INFO",6370997.0,0.0]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.017453292519943295]],PROJECTION["Lambert_Azimuthal_Equal_Area"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-100.0],PARAMETER["Latitude_Of_Origin",45.0],UNIT["Meter",1.0]]'
 
 gdalwarp -overwrite -tr 30 30 -t_srs EPSG:42303 -co "PROFILE=GeoTIFF" -co "TILED=YES" -co "SPARSE_OK=TRUE" -co "COMPRESS=LZW" -co "NUM_THREADS=ALL_CPUS" -s_srs $ssrs -r $1 $2 $3
+
+
+ssrs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,-0,-0,-0,0 +units=m +no_defs"
+gdalwarp -overwrite -tr 30 30 -t_srs EPSG:42303 -co "PROFILE=GeoTIFF" -co "TILED=YES" -co "SPARSE_OK=TRUE" -co "COMPRESS=LZW" -co "NUM_THREADS=ALL_CPUS" -s_srs $ssrs
