d8049d2f1c37522b856d2095cefbe6f91b298428
Theodore Barnhart <tbarnhart@usgs.gov>
2018-10-05 16:22:06 -0600

    switching to using r.watershed with hydroDEM instead of r.accumulate for memory efficiency.

--- make_cpg.sh
+++ make_cpg.sh
@@ -3,9 +3,11 @@
 reg=$1
 inpath='/home/tbarnhart/projects/DEM_processing/data/cpg_datasets' # path for source data files
 echo Linking to External Data
-r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21_facfdr/region_${reg}_fac.vrt out=accum --overwrite --quiet -o # link the NHDplus accumulation grid
-r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21_facfdr/region_${reg}_fdr_grass.tiff out=dir --overwrite --quiet -o # link the NHDplus direction grid
-g.region rast=dir res=30 # explicitely set region to imported drainage dir extent
+#r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21_facfdr/region_${reg}_fac.vrt out=accum --overwrite --quiet -o # link the NHDplus accumulation grid
+#r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21_facfdr/region_${reg}_fdr_grass.tiff out=dir --overwrite --quiet -o # link the NHDplus direction grid
+r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21/region_${reg}.vrt out=elev --overwrite --quiet -o # link to NHD hydroDEM
+g.region rast=elev res=30 # explicitely set region to imported drainage dir extent
+r.watershed -m --overwrite --quiet elevation=elev accumulation=accum memory=115000 # generate accumulation from elevation
 r.mapcalc --overwrite "accum_fill = accum + 1" # add one to the flow accumulation grid to help w/ math down the line
 echo Linking Complete
 for inDat in `ls -1 ${inpath}/*.tiff`; do # iterate through the source data files in the CPG directory, all tiffs in directory...
@@ -19,8 +21,11 @@ for inDat in `ls -1 ${inpath}/*.tiff`; do # iterate through the source data file
     nd="$(cut -d "=" -f2 <<<$tmp)" # extract the no data value
     
     r.external in=${inDat} out=param --overwrite --quiet -o # link the parameter grid
-    r.mapcalc "param_fill = if(param==${nd},0,param)" --overwrite # fill nulls with zero before accumulating the parameter surface 
-    r.accumulate dir=dir weight=param_fill accum=param_accum --overwrite --quiet # accumulate the parameter based on the flow direction grid
+    r.mapcalc "param_fill = if(param==${nd},0,param)" --overwrite # fill nulls with zero before accumulating the parameter surface
+    #r.accumulate dir=dir weight=param_fill accum=param_accum --overwrite --quiet # accumulate the parameter based on the flow direction grid
+
+    r.watershed -m --overwrite --quiet elevation=elev flow=param_fill accumulation=param_accum memory=115000 # accumulate the parameter
+ 
     r.mapcalc "param_cpg = float(param_accum) / float(accum_fill)" --overwrite # compute the actual CPG
     echo Saving to: $outDat
     r.out.gdal --quiet --overwrite in=param_cpg out=${outDat} format=GTiff createopt="TILED=YES,COMPRESS=LZW,NUM_THREADS=ALL_CPUS,SPARSE_OK=TRUE,PROFILE=GeoTIFF" # export the CPG
