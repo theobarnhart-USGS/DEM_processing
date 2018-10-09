f1dd244aac552e680852b631e77f2776c00cdff5
Theodore Barnhart <tbarnhart@usgs.gov>
2018-10-09 09:36:12 -0600

    changed to single flow direction and positive flow only.

--- make_cpg.sh
+++ make_cpg.sh
@@ -3,11 +3,10 @@
 reg=$1
 inpath='/home/tbarnhart/projects/DEM_processing/data/cpg_datasets' # path for source data files
 echo Linking to External Data
-#r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21_facfdr/region_${reg}_fac.vrt out=accum --overwrite --quiet -o # link the NHDplus accumulation grid
-#r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21_facfdr/region_${reg}_fdr_grass.tiff out=dir --overwrite --quiet -o # link the NHDplus direction grid
+
 r.external in=/home/tbarnhart/projects/DEM_processing/data/NHDplusV21/region_${reg}.vrt out=elev --overwrite --quiet -o # link to NHD hydroDEM
 g.region rast=elev res=30 # explicitely set region to imported drainage dir extent
-r.watershed -m --overwrite --quiet elevation=elev accumulation=accum memory=115000 # generate accumulation from elevation
+r.watershed -msa --overwrite --quiet elevation=elev accumulation=accum memory=115000 # generate accumulation from elevation
 r.mapcalc --overwrite "accum_fill = accum + 1" # add one to the flow accumulation grid to help w/ math down the line
 echo Linking Complete
 for inDat in `ls -1 ${inpath}/*.tiff`; do # iterate through the source data files in the CPG directory, all tiffs in directory...
@@ -22,9 +21,8 @@ for inDat in `ls -1 ${inpath}/*.tiff`; do # iterate through the source data file
     
     r.external in=${inDat} out=param --overwrite --quiet -o # link the parameter grid
     r.mapcalc "param_fill = if(param==${nd},0,param)" --overwrite # fill nulls with zero before accumulating the parameter surface
-    #r.accumulate dir=dir weight=param_fill accum=param_accum --overwrite --quiet # accumulate the parameter based on the flow direction grid
 
-    r.watershed -m --overwrite --quiet elevation=elev flow=param_fill accumulation=param_accum memory=115000 # accumulate the parameter
+    r.watershed -msa --overwrite --quiet elevation=elev flow=param_fill accumulation=param_accum memory=115000 # accumulate the parameter
  
     r.mapcalc "param_cpg = float(param_accum) / float(accum_fill)" --overwrite # compute the actual CPG
     echo Saving to: $outDat
