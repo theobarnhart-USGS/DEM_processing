fa8d5dee9fdec8fb9fb2966811d60dedd7c8ae4b
Theodore Barnhart <tbarnhart@usgs.gov>
2018-10-17 12:43:49 -0600

    tweaking of SBATCH params

--- submit_make_cpg_tauDEM.sh
+++ submit_make_cpg_tauDEM.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=CPG # name that you chose
 #SBATCH -c 1 # cpus per task
-#SBATCH -n 21 # number of tasks
+#SBATCH -n 4 # number of tasks
+#SBATCH --tasks-per-node=4
 #SBATCH -p normal                         # the partition you want to use, for this case prod is best
 #SBATCH --account=wymtwsc        # your account
 #SBATCH --time=08:00:00           # Overestimated guess at time
 #SBATCH --mail-type=ALL         # Send email on all events
 #SBATCH --mail-user=tbarnhart@usgs.gov
-#SBATCH  -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)  
+#SBATCH -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)  
+#SBATCH -e %j.err
 #SBATCH --mem=128000            #memory in MB 
 
 source activate py36
